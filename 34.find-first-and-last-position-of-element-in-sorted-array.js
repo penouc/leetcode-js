070e28bc5061d0a8b94d85659e8a68263db34d98
pl <penouc@gmail.com>
2019-05-05 17:25:10 +0800

    feat: 增加二分查找 --> 寻找一个包含当前字符的区间

--- 34.find-first-and-last-position-of-element-in-sorted-array.js
+++ 34.find-first-and-last-position-of-element-in-sorted-array.js
@@ -37,3 +37,42 @@ var searchRange = function(nums, target) {
 
   return ret.length === 2 ? ret : [-1, -1];
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let ret = [-1, -1];
+
+  while (left < right) {
+    let mid = parseInt((left + right) / 2);
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  if (nums[left] !== target) {
+    return ret;
+  } else {
+    ret[0] = left;
+  }
+
+  right = nums.length - 1;
+  while (left < right) {
+    let mid = parseInt((left + right) / 2) + 1;
+    if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid;
+    }
+  }
+  ret[1] = right;
+
+  return ret;
+};
