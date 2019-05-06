24877fd1689641d6e806f6b898d0f97dea76559a
彭磊 <penouc@gmail.com>
2019-05-07 01:59:04 +0800

    feat: 981 hack 没有用到二分查找

--- 981.time-based-key-value-store.js
+++ 981.time-based-key-value-store.js
@@ -15,52 +15,50 @@
  * Initialize your data structure here.
  */
 class TimeMap {
-    constructor() {
-      this.data = {};
-    }
-  
-    set(key, value, timestamp) {
-      if (this.data[key]) {
-        let arr = this.data[key];
-        arr[timestamp] = value;
-      } else {
-        let arr = [];
-        arr[timestamp] = value;
-        this.data[key] = arr;
-      }
-    }
-  
-    get(key, timestamp) {
-      if (this.data.hasOwnProperty(key)) {
-        let arr = this.data[key];
-  
-        let i = timestamp;
-  
-        // just find the smaller index from the current timestamp
-        // worse case is O(timestamp) look up time.
-        while (i > 0) {
-          if (typeof arr[i] !== "undefined") {
-            return arr[i];
-          }
-          i--;
-        }
-      }
-      return "";
+  constructor() {
+    this.data = {};
+  }
+
+  set(key, value, timestamp) {
+    if (this.data[key]) {
+      let arr = this.data[key];
+      arr[timestamp] = value;
+    } else {
+      let arr = [];
+      arr[timestamp] = value;
+      this.data[key] = arr;
     }
   }
-  
-  /**
-   * Your TimeMap object will be instantiated and called as such:
-   * var obj = new TimeMap()
-   * obj.set(key,value,timestamp)
-   * var param_2 = obj.get(key,timestamp)
-   */
-  
 
-/** 
+  get(key, timestamp) {
+    if (this.data.hasOwnProperty(key)) {
+      let arr = this.data[key];
+
+      let i = timestamp;
+
+      // just find the smaller index from the current timestamp
+      // worse case is O(timestamp) look up time.
+      while (i > 0) {
+        if (typeof arr[i] !== "undefined") {
+          return arr[i];
+        }
+        i--;
+      }
+    }
+    return "";
+  }
+}
+
+/**
  * Your TimeMap object will be instantiated and called as such:
  * var obj = new TimeMap()
  * obj.set(key,value,timestamp)
  * var param_2 = obj.get(key,timestamp)
  */
 
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * var obj = new TimeMap()
+ * obj.set(key,value,timestamp)
+ * var param_2 = obj.get(key,timestamp)
+ */
