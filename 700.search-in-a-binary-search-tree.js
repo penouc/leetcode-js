06355f5df3a12f9f1e1f2487dbc371f58ee65135
彭磊 <penouc@gmail.com>
2019-05-13 00:24:29 +0800

    feat: 找到一颗二叉排序树中的值相等的节点

---
+++ 700.search-in-a-binary-search-tree.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=700 lang=javascript
+ *
+ * [700] Search in a Binary Search Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+  if (root === null) {
+    return null;
+  }
+
+  if (val === root.val) {
+    return root;
+  } else if (val < root.val) {
+    return searchBST(root.left, val);
+  } else if (val > root.val) {
+    return searchBST(root.right, val);
+  }
+};
