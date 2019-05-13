2b3619494da8ba87fed24af57d26926c8ac231e2
pl <penouc@gmail.com>
2019-05-13 11:14:07 +0800

    feat: 增加用 dfs 算法解决排序二叉树插入的问题

--- 701.insert-into-a-binary-search-tree.js
+++ 701.insert-into-a-binary-search-tree.js
@@ -36,3 +36,23 @@ var insertIntoBST = function(root, val) {
 
     return ori
 };
+
+
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function(root, val) {
+    if(root === null) {
+        return new TreeNode(val)
+    }
+
+    if(val > root.val) {
+        root.right = insertIntoBST(root.right, val);
+    }else {
+        root.left = insertIntoBST(root.left, val);
+    }
+
+    return root
+};
