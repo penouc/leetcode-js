06efff400e07350afe30ab08460462a1f5d5436c
彭磊 <penouc@gmail.com>
2019-05-16 23:54:23 +0800

    feat: 实现组合算法,关键点在 push slice 和 pop

---
+++ 77.combinations.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=77 lang=javascript
+ *
+ * [77] Combinations
+ */
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+  let res = [];
+
+  function helper(startInx, sub) {
+    if (sub.length === k) {
+      res.push(sub.slice());
+      return;
+    }
+
+    for (let i = startInx; i <= n; i++) {
+      if (sub.length >= 1 && sub[sub.length - 1] >= i) continue;
+      sub.push(i);
+      helper(startInx + 1, sub);
+      sub.pop();
+    }
+  }
+  helper(1, []);
+  return res;
+};
