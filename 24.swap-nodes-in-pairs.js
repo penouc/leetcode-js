e95ac7c96f2c901dcc88d02fb19837e545ba2240
彭磊 <penouc@gmail.com>
2019-05-21 01:32:43 +0800

    feat:  链表中相邻两项置换 这个解法太牛逼了

--- 24.swap-nodes-in-pairs.js
+++ 24.swap-nodes-in-pairs.js
@@ -40,3 +40,17 @@ var swapPairs = function(head) {
 
   return ret.next;
 };
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+  if (head === null || head.next === null) {
+    return head;
+  }
+  let n = head.next;
+  head.next = swapPairs(head.next.next);
+  n.next = head;
+  return n;
+};
