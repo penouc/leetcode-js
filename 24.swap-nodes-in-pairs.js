36afc722e0aa2e6f3e7e4427dfcaa44aa6a8ee3c
彭磊 <penouc@gmail.com>
2019-05-21 01:25:51 +0800

    feat:  链表中相邻两项置换

---
+++ 24.swap-nodes-in-pairs.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=24 lang=javascript
+ *
+ * [24] Swap Nodes in Pairs
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+  let ret = new ListNode(0);
+  let flag = 0;
+  let pre = head;
+  let temp = ret;
+
+  while (head) {
+    if (flag === 0) {
+      flag++;
+      head = head.next;
+    } else {
+      temp.next = new ListNode(head.val);
+      temp = temp.next;
+      temp.next = new ListNode(pre.val);
+      temp = temp.next;
+      pre = head.next;
+      head = head.next;
+      flag--;
+    }
+  }
+  if (pre && (pre.val || pre.val === 0)) {
+    temp.next = new ListNode(pre.val);
+  }
+
+  return ret.next;
+};
