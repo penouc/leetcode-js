a17bc88aa13cc7f866a42b3bab4e214258df5dd0
彭磊 <penouc@gmail.com>
2019-05-23 01:38:07 +0800

    feat: 快速模式

--- 189.rotate-array.js
+++ 189.rotate-array.js
@@ -14,3 +14,26 @@ var rotate = function(nums, k) {
   }
   return nums;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  k %= nums.length;
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+};
+
+function reverse(nums, start, end) {
+  while (start < end) {
+    let temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+
+    start++;
+    end--;
+  }
+}
