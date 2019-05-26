176668c3efd5d3a3a544de6fab02b1688c2bb5d1
彭磊 <penouc@gmail.com>
2019-05-27 00:49:46 +0800

    feat: 最佳买入和卖出股票的时机

---
+++ 121.best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=121 lang=javascript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let minprice = Number.POSITIVE_INFINITY;
+  let maxprofit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minprice) {
+      minprice = prices[i];
+    } else if (prices[i] - minprice > maxprofit) {
+      maxprofit = prices[i] - minprice;
+    }
+  }
+
+  return maxprofit;
+};
