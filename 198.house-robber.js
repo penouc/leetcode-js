8eb158e634963ba63155105f7f48abe7146a191c
彭磊 <penouc@gmail.com>
2019-05-27 01:09:05 +0800

    feat: 数组中打劫问题

---
+++ 198.house-robber.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=198 lang=javascript
+ *
+ * [198] House Robber
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  let a = 0;
+  let b = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      a = Math.max(a + nums[i], b);
+    } else {
+      b = Math.max(b + nums[i], a);
+    }
+  }
+
+  return Math.max(a, b);
+};
