470030c092ff0e6c239e1b31f9ba101ff316cc87
彭磊 <penouc@gmail.com>
2019-05-30 00:23:54 +0800

    feat: 寻找一个数组中存在的最大增长子序列

---
+++ 300.longest-increasing-subsequence.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=300 lang=javascript
+ *
+ * [300] Longest Increasing Subsequence
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  let dp = Array(nums.length).fill(0);
+  dp[0] = 1;
+  let maxans = 1;
+
+  for (let i = 1; i < dp.length; i++) {
+    let maxval = 0;
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        maxval = Math.max(maxval, dp[j]);
+      }
+    }
+    dp[i] = maxval + 1;
+    maxans = Math.max(maxans, dp[i]);
+  }
+
+  return maxans;
+};
